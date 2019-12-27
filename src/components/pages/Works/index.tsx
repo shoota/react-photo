6312a7f1c91201d31a09834f565d9611fa4c2561
Shuta Kumano <shoota.dev@gmail.com>
2019-12-27 15:41:09 +0900

    implement works viewer

--- src/components/pages/Works/index.tsx
+++ src/components/pages/Works/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { List, Card } from 'antd'
+import { List, Card, Modal } from 'antd'
 import styled from 'styled-components'
 
 const StyledCard = styled(Card)`
@@ -10,32 +10,79 @@ const StyledCard = styled(Card)`
 
 type WorkData = {
   title: string
-  thumbnail: string
   content: string
-  onClick: (e: React.MouseEvent<HTMLAnchorElement>) => void
+  files: string[]
 }
 
 type Props = {
+  currentImageKey: number
+  // FIXME should not load all work data. it should fetch dinamitically
   datasource: WorkData[]
+  onListClick: (e: React.MouseEvent<HTMLAnchorElement>) => void
+  onImageClick: (e: React.MouseEvent<HTMLAnchorElement>) => void
+  visibleWork: WorkData
+  visible: boolean
 }
 
-export const Works: React.FC<Props> = ({ datasource }) => {
+const Photo = styled.div`
+  text-align: center;
+  margin-bottom: 24px;
+  img {
+    border: solid 2px #202020;
+    border-radius: 3px;
+    cursor: pointer;
+  }
+`
+
+export const Works: React.FC<Props> = ({
+  currentImageKey,
+  datasource,
+  onImageClick,
+  onListClick,
+  visibleWork,
+  visible,
+}) => {
   return (
     <StyledCard>
       <List
         itemLayout="vertical"
         size="large"
         dataSource={datasource}
-        renderItem={({ title, thumbnail, content }) => (
+        renderItem={({ title, files, content }) => (
           <List.Item
             key={title}
-            extra={<img width={220} alt="logo" src={thumbnail} />}
+            extra={
+              <a href="/" onClick={onListClick}>
+                <img width={220} alt="logo" src={files[0]} />
+              </a>
+            }
           >
-            <List.Item.Meta title={title} />
+            <List.Item.Meta
+              title={
+                <a href="/" onClick={onListClick}>
+                  title
+                </a>
+              }
+            />
             {content}
           </List.Item>
         )}
       />
+      <Modal
+        width={720}
+        title={visibleWork.title}
+        visible={visible}
+        footer={null}
+      >
+        <Photo>
+          <a href="/" onClick={onImageClick}>
+            <img
+              alt={visibleWork.title}
+              src={visibleWork.files[currentImageKey]}
+            />
+          </a>
+        </Photo>
+      </Modal>
     </StyledCard>
   )
 }
